244238b2b6deb737592a46b7950ee2f67b204261
chaco470 <marianolopez.com.26@gmail.com>
2021-10-22 19:24:00 -0300

    puertas implementadas

--- Assets/Scenes/Puerta.gd
+++ Assets/Scenes/Puerta.gd
@@ -1,5 +1,16 @@
 extends Sprite
+onready var barrera = $StaticBody2D
 
 
 func abrir():
 	queue_free()
+
+
+func _on_Area2D_body_entered(body):
+	barrera.set_collision_layer(0)
+	barrera.set_collision_mask(0)
+
+
+func _on_Area2D_body_exited(body):
+	barrera.set_collision_layer(1)
+	barrera.set_collision_mask(1)
--- Assets/Scenes/World.gd
+++ Assets/Scenes/World.gd
@@ -1,10 +1,13 @@
 extends Node
 onready var enemigos_s_1 = $Enemigos_sala_1
+onready var enemigos_s_2 = $Enemigos_sala_2
 onready var puerta1 = $Puerta
 onready var puerta2 = $Puerta2
+onready var puerta3 = $Puerta3
+onready var puerta4 = $Puerta4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	enemigos_s_1.initialize(puerta1, puerta2)
-
+	enemigos_s_2.initialize(puerta3, puerta4)
 
